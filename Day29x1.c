5c8884cf4855a91ce843b1c18b19462933f9e124
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-10-28 23:59:58 +0530

    Create Day29x1.c

---
+++ Day29x1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {64, 25, 12, 22, 11}, n = 5;
+    for (int i=0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) min_idx = j;
+        }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+    printf("Sorted array: ");
+    for(int i=0; i<n; i++) printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
