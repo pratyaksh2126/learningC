bdf6ff43893f1a5c04a1fdd31b877802a9d75a86
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-10-29 00:00:33 +0530

    Create Day30x2.c

---
+++ Day30x2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
+    int len = strlen(str) - 1; // remove newline
+    for(int i = 0; i < len/2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
