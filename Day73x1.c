75c37bd587369d49c4af1fc1779d91503841f402
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-11-04 12:50:47 +0530

    Create Day73x1.c

---
+++ Day73x1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int age;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    printf("Enter student name: ");
+    gets(s.name);
+    printf("Enter age: ");
+    scanf("%d", &s.age);
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    printf("\nStudent Details:\n");
+    printf("Name: %s\n", s.name);
+    printf("Age: %d\n", s.age);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
