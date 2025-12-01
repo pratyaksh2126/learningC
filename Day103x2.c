e159f5eae120c2b24f52da20b52580ada1ff169c
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-12-01 20:16:21 +0530

    Create Day103x2.c

---
+++ Day103x2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+    int vowels = 0, consonants = 0, digits = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+        else if (ch >= '0' && ch <= '9') {
+            digits++;
+        }
+    }
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+    printf("Digits: %d\n", digits);
+
+    return 0;
+}
