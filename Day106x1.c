c9125125dea41de4b6421761cd4ba856a618a116
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-12-04 11:54:46 +0530

    Create Day106x1.c

---
+++ Day106x1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = str[i];
+        if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+            c=='A'||c=='E'||c=='I'||c=='O'||c=='U') {
+            count++;
+        }
+    }
+
+    printf("Number of vowels = %d", count);
+    return 0;
+}
