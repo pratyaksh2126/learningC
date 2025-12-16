1ecc799daa18d0a4999fda814f1c2e34b75640bf
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-12-16 21:24:24 +0530

    Create Day117x1.c

---
+++ Day117x1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, max;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
