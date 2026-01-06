1b5bd69a70d588b21b21142efeeec301f28bc854
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-01-06 22:50:58 +0530

    Create Day141x1.c

---
+++ Day141x1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0], min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Largest element = %d\n", max);
+    printf("Smallest element = %d\n", min);
+
+    return 0;
+}
