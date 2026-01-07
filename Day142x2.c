08849f043cfe897f6c6ef66f60be7a6a94a02a79
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-01-07 20:48:57 +0530

    Create Day142x2.c

---
+++ Day142x2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct student s;
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.roll);
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    printf("Enter Marks: ");
+    scanf("%f", &s.marks);
+
+    printf("\n--- Student Details ---\n");
+    printf("Roll No: %d\n", s.roll);
+    printf("Name: %s\n", s.name);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
