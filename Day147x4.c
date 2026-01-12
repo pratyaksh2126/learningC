26a14e8f350231d073db0cf4e35f111c44c155ab
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-01-12 18:30:56 +0530

    Create Day147x4.c

---
+++ Day147x4.c
@@ -0,0 +1,13 @@
+// Write a program to add two numbers //
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a, b ;
+    printf("Enter First No.: " );
+    scanf("%d",&a);
+    printf("Enter Second No.:  " );
+    scanf("%d",&b);
+    printf("The Sum of %d and %d : %d ",a, b, a+b);
+    return 0;
+
+}
