7743fbd152323ff956be5868bcc6002a9a8a434a
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-01-12 18:30:59 +0530

    Create Day147x5.c

---
+++ Day147x5.c
@@ -0,0 +1,10 @@
+// Write a program to print numbers from 1 to 10 using for loop //
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int i; 
+    for(i=1;i<=10;i++){
+        printf("%d \n",i);
+    }
+    return 0;
+}
