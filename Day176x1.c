ff6311aa445ecdddfb40799619f481c538616cc8
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-10 10:21:29 +0530

    Create Day176x1.c

---
+++ Day176x1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int k = 0;
+
+    // Move non-zero elements forward
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[k] = arr[i];
+            k++;
+        }
+    }
+
+    // Fill remaining with zeros
+    for (int i = k; i < n; i++) {
+        arr[i] = 0;
+    }
+
+    // Print result
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
