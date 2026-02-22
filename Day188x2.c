0314e88eab72e242ed7072b9b2a31fa3d5b34910
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-22 21:45:11 +0530

    Create Day188x2.c

---
+++ Day188x2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = INT_MIN, secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } 
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == INT_MIN)
+        printf("No Second Largest Element");
+    else
+        printf("Second Largest: %d", secondLargest);
+
+    return 0;
+}
